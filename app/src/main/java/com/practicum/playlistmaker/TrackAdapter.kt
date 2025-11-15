02b26acdf74f22eb28e299bf1a492fceb2da811a
Shaxtyr <shaxtyr@yandex.ru>
2025-11-16 00:27:23 +0300

    Sprint14_c33

--- app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
@@ -1,11 +1,16 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TrackAdapter(private val tracks: List<Track>, private val searchHistory: SearchHistory, private val isHistoryTrack: Boolean) : RecyclerView.Adapter<TrackViewHolder>() {
+
+    private var isClickAllowed = true
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -19,26 +24,36 @@ class TrackAdapter(private val tracks: List<Track>, private val searchHistory: S
         holder: TrackViewHolder,
         position: Int
     ) {
-        holder.bind(tracks[position])
         holder.itemView.setOnClickListener {
-
-            if (!isHistoryTrack) {
-                searchHistory.addToHistory(tracks[position])
+            if (clickDebounce()) {
+                if (!isHistoryTrack) {
+                    searchHistory.addToHistory(tracks[position])
+                }
+                val context = holder.itemView.context
+                val audioPlayerIntent = Intent(context, AudioPlayerActivity::class.java)
+                audioPlayerIntent.putExtra(OPEN_TRACK_KEY, tracks[position])
+                context.startActivity(audioPlayerIntent)
             }
-
-            val context = holder.itemView.context
-            val audioPlayerIntent = Intent(context, AudioPlayerActivity::class.java)
-            audioPlayerIntent.putExtra(OPEN_TRACK_KEY, tracks[position])
-            context.startActivity(audioPlayerIntent)
         }
+
+        holder.bind(tracks[position])
     }
 
     override fun getItemCount(): Int {
         return tracks.size
     }
 
-    companion object {
-        const val OPEN_TRACK_KEY = "open_track"
+    private fun clickDebounce(): Boolean {
+        val current = isClickAllowed
+        if (isClickAllowed) {
+            isClickAllowed = false
+            handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY)
+        }
+        return current
     }
 
+    companion object {
+        const val OPEN_TRACK_KEY = "open_track"
+        const val CLICK_DEBOUNCE_DELAY = 1000L
+    }
 }
